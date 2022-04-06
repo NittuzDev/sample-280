8419570a4056154001501c978eb2175dc8959d9a
Nicola Bertoli <nicolabertoli@Nicolas-MacBook-Pro.local>
2022-04-06 11:30:28 +0200

    second release

--- setup.sh
+++ setup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-oc adm
 
-#creation of scc exam sample
+lab install-troubleshoot start
+
+# taint nodes
+oc adm taint nodes master01 deploy:here:NoSchedule
+oc label node master01 deploy=here
+oc adm taint nodes master02 dev:restricted:NoSchedule
+oc adm taint nodes master03 dev:restricted:NoSchedule
+
+# nodeselector 
+oc new-project nginx-app
+
+# creation of scc exam sample
 oc new-project test-scc
 oc new-app --name gitlab --docker-image quay.io/redhattraining/gitlab-ce:8.4.3-ce.0
 
