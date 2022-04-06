d62021e7eb3e3ba0c6c4a65c84543b8020afa4cf
Nicola Bertoli <nicolabertoli@Nicolas-MacBook-Pro.local>
2022-04-06 12:31:38 +0200

    updated files

--- setup.sh
+++ setup.sh
@@ -12,14 +12,20 @@ oc label node master01 deploy=here
 oc adm taint nodes master02 dev=restricted:NoSchedule
 oc adm taint nodes master03 dev=restricted:NoSchedule
 
-# nodeselector
+# nodeselector problem
 oc new-project nginx-app
 oc apply -f nginx-app/deployment.yaml
 oc apply -f nginx-app/service.yaml
 oc expose svc/nginx
 
-# creation of scc exam sample
+# scc problem
 oc new-project test-scc
 oc new-app --name gitlab --docker-image quay.io/redhattraining/gitlab-ce:8.4.3-ce.0
+
+#memory exceeded + secret
+oc new-project php-app
+oc new-app --name php https://github.com/nicola-bertoli/sample-280 --context-dir php-app
+oc set resources deployment php --requests=memory=80Gi
+
 
 oc project default
