28b231f627c3937215c442884396f21fd8ffd20e
Nicola Bertoli <nicolabertoli@Nicolas-MacBook-Pro.local>
2022-04-06 12:07:55 +0200

    fixed some bugs

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 #lab install-troubleshoot start
 
 source /usr/local/etc/ocp4.config
-echo ${RHT_OCP4_KUBEADM_PASSWD} >> /home/student/Documents
+echo ${RHT_OCP4_KUBEADM_PASSWD} >> /home/student/Documents/master_pass.txt
 oc login -u kubeadmin -p ${RHT_OCP4_KUBEADM_PASSWD} ${RHT_OCP4_MASTER_API}
 
 # taint nodes
@@ -14,10 +14,12 @@ oc adm taint nodes master03 dev=restricted:NoSchedule
 
 # nodeselector
 oc new-project nginx-app
-oc apply -f nginx-app/.
+oc apply -f nginx-app/deployment.yaml
+oc apply -f nginx-app/service.yaml
 oc expose svc/nginx
 
 # creation of scc exam sample
 oc new-project test-scc
 oc new-app --name gitlab --docker-image quay.io/redhattraining/gitlab-ce:8.4.3-ce.0
 
+oc project default
